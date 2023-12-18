ca26357db66b8ad331382c8128828b1b8eb3e784
Vinayak <myselfvinayakhegde@gmail.com>
2023-12-18 12:38:04 +0530

    feat: add metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,35 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Convio - Free Unlimited File Converter",
   description: "One converter for all your needs.",
+  creator: "Vinayka Hegde",
+  keywords: [
+    "image converter",
+    "audio converter",
+    "video converter",
+    "unlimited video converter",
+    "unlimited audio converter",
+    "unlimited image converter",
+    "best image converter free",
+  ],
 };
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <div className="pt-32 min-h-screen lg:pt-36 2xl:pt-44 container max-w-4xl lg:max-w-6xl 2xl:max-w-7xl"></div>
+      </body>
     </html>
-  )
+  );
 }
